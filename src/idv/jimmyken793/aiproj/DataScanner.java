b2535f49a1527abd492fda0250210b3e2cb11ccd
jimmy <jimmy@Jimmy-T400.(none)>
2010-06-13 21:08:15 +0800

    added usage message

--- src/idv/jimmyken793/aiproj/DataScanner.java
+++ src/idv/jimmyken793/aiproj/DataScanner.java
@@ -24,15 +24,24 @@ public class DataScanner {
 	 * @throws SQLException
 	 */
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
-		if (args.length ==0)
+		if (args.length == 0) {
+			System.out.println("Usage:");
+			System.out.println("java -jar Datascanner.jar -Data_Mode Data_path_list [-Data_Mode Data_path_list]");
+			System.out.println("Data modes:");
+			System.out.println("PidginLog : pidgin conversation log");
+			System.out.println("MsnLog : MSN Converstion Log");
+			System.out.println("MsnXmlLog : MSN Conversation Log(XML Format)");
+			System.out.println("UdnNews : UDN News page");
+			System.out.println("Data : Raw data");
 			return;
-		
+		}
+
 		Database d = new Database();
-		String className="Data";
+		String className = "Data";
 		for (int i = 0; i < args.length; i++) {
-			if(args[i].charAt(0)=='-'){
-				className=args[i].substring(1, args[i].length());
-				System.out.println("Changed into "+className+" mode");
+			if (args[i].charAt(0) == '-') {
+				className = args[i].substring(1, args[i].length());
+				System.out.println("Changed into " + className + " mode");
 				continue;
 			}
 			ArrayList<String> files = FileIO.FileList(args[i]);
